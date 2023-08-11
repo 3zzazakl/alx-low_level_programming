87271584eae2534c03e5d44a3fdfed6f410f0b83
3zzazakl <vanzekovichgr@gmail.com>
2023-08-11 05:48:08 +0300

    New

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
 
-
 /**
  * main - Printing a specific statement
  *
@@ -9,6 +8,6 @@
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
