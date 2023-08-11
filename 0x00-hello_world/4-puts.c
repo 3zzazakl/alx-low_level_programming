a0aa299f26ec9811e6fde042a990bffc61b5b3a6
3zzazakl <vanzekovichgr@gmail.com>
2023-08-11 04:56:00 +0300

    Assembly code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,5 +8,11 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return (0);	
 }
+/**
+ * main - hammififklkfkldglkgfnkgr;klhn
+ * 
+ * Return: (Success) 0
+ * 
+*/
