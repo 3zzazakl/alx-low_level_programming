be433c0fe9700c861d4fed0c661d40566e0f222f
3zzazakl <vanzekovichgr@gmail.com>
2023-08-12 05:07:40 +0300

    numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - printing numbere from 0 to 9 with ,
+ *
+ * Return: (Success) 0
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar('0' + num);
+		if (num == 9)
+			break;
+		putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
