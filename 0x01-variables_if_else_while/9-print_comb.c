6dbe48e94a4f56d591a934d35c9591a39139a893
3zzazakl <vanzekovichgr@gmail.com>
2023-08-12 05:13:33 +0300

    numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,10 +11,11 @@ int main(void)
 
 	for (num = 0; num < 10; num++)
 	{
-		putchar('0' + num);
+		putchar('0'+ num);
 		if (num == 9)
 			break;
 		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
