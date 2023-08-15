9b90ea1787fc97339b0fa35ce9b5e0e7d08b4494
3zzazakl <vanzekovichgr@gmail.com>
2023-08-15 17:29:24 +0300

    Last digit

---
+++ 0x02-functions_nested_loops/test/hours.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print hours 
+*/
+void main(void)
+{
+       int x, y;
+
+       for (x = 00; )
+
+}
---
+++ 0x02-functions_nested_loops/test/last.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+       int x = 99998;
+
+       printf("The last digit is: %d\n", (x % 10));
+       return (0);
+}
