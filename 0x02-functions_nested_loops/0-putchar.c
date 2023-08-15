4aeffe138c7f0bb9e97dcfcf692270b424ae7f97
3zzazakl <vanzekovichgr@gmail.com>
2023-08-15 13:45:19 +0300

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
  * main - Printing _putchar
  *
@@ -7,6 +6,13 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char alpha[] = "_putchar";
+	int index;
+
+	for (index = 0; index < 8; index++)
+	{
+		_putchar(alpha[index]);
+	}
+	_putchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,7 +10,7 @@ void print_alphabet(void)
 
 	for (alph = 'a'; alph <= 'z'; alph++)
 	{
-		putchar(alph);
+		_putchar(alph);
 	}
 	putchar('\n');
 }
