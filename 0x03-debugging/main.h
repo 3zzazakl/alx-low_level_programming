f17bc42d3a55a7619109863e263adf4eeecd50a8
3zzazakl <vanzekovichgr@gmail.com>
2023-08-16 19:24:43 +0300

    Main file

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Headers Declaration */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* Function Declarations */
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
