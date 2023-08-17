8eda9b5351539c0d07170f2a543c3f95f58094bf
3zzazakl <vanzekovichgr@gmail.com>
2023-08-17 03:06:25 +0300

    Time Table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table, starting with 0.
+ * @n: The input value.
+*/
+
+void print_times_table(int n)
+{
+	int x, y, mul;
+
+	if (n >= 0 && n <= 14)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			for (y = 0; y <= n; y++)
+			{
+				mul = x * y;
+				if (mul > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((mul / 100) + '0');
+					_putchar(((mul / 10) % 10) + '0');
+					_putchar(( mul% 10) + '0');
+				}
+				else if (mul > 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(((mul / 10) % 10) + '0');
+					_putchar((mul % 10) + '0');
+				}
+				else
+				{
+					if (y != 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar(mul + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,7 +22,6 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(mul + '0');
-
 			}
 			else
 			{
