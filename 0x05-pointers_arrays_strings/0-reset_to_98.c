daf0e2964cbd1edc896f7576556f2cb1f6760634
3zzazakl <vanzekovichgr@gmail.com>
2023-08-22 02:11:54 +0300

    98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updating the value of variable using pointers.
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Headers_Declarations */
+#include <stdio.h>
+
+
+/* Function Declarations */
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
