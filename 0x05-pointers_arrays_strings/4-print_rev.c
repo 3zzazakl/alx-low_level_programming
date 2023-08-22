ea84c0b0dccae0cff5bfb6113fc5fa6f704a4655
3zzazakl <vanzekovichgr@gmail.com>
2023-08-22 05:39:57 +0300

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * print_rev - prints a string, in reverse.
+ * @s: Input string.
 */
 void print_rev(char *s)
 {
-       int x, length;
+	int x, length;
 
-       length = strlen(s);
+	length = strlen(s);
 
 	for (x = length; x >= 0; x--)
 	{
