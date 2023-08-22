1cfa4ccc96fcbf62f8b20d6c30a4b018f3befbae
3zzazakl <vanzekovichgr@gmail.com>
2023-08-22 04:17:00 +0300

    Length

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,10 +3,11 @@
 
 /* Headers_Declarations */
 #include <stdio.h>
-
+#include <string.h>
 
 /* Function Declarations */
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 
 #endif /* MAIN_H */
