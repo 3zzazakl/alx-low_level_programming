ddc8ca2758ffbe4ec77beeb7031ad63626c9fef6
3zzazakl <vanzekovichgr@gmail.com>
2023-08-24 23:20:19 +0300

    Concat2

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,9 +5,9 @@
 #include <stdio.h>
 #include <string.h>
 
-
 /* Functions_Declarations */
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 
 #endif /* MAIN_H */
  
