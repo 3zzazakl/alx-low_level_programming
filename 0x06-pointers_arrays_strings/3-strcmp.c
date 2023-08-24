5c37aebc797fd3753373d4e65bcab61916f9ec72
3zzazakl <vanzekovichgr@gmail.com>
2023-08-25 01:47:01 +0300

    Compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,43 +1,17 @@
 #include "main.h"
+
 /**
  * _strcmp - compares two strings.
- * @s1: First String
- * @s2: Second String
- * Return: (number of compared characters).
+ * @s1: First String.
+ * @s2: Second String.
+ * Return: (The compared Numbers).
 */
 int _strcmp(char *s1, char *s2)
 {
-	int i, len, len1 = 0, len2 = 0;
-
-	for (i = 0; s1[i] != '\0'; i++)
-		len1++;
-	for (i = 0; s2[i] != '\0'; i++)
-		len2++;
-
-	len = (len1 > len2) ? len1 : len2;
-
-	for (i = 0; i < len; i++)
+	while (*s1 && (*s1 == *s2))
 	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-			break;
-		}
-		else if (s1[i] == s2[i])
-		{
-			if (s1[i + 1] == '\0' && s2[i + 1] != '\0')
-			{
-				return (-32);
-			}
-			if (s1[i + 1] != '\0' && s2[i + 1] == '\0')
-			{
-				return (32);
-			}
-			if (s1[i + 1] == '\0' && s2[i + 1] == '\0')
-			{
-				return (0);
-			}
-			break;
-		}
+		s1++;
+		s2++;
 	}
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
