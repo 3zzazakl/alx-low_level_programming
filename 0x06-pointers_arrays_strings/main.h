e3fb04e870a6f80daae806a1ddf664147471595d
3zzazakl <vanzekovichgr@gmail.com>
2023-08-24 19:57:38 +0300

    Concat

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Headers_Declarations */
+#include <stdio.h>
+#include <string.h>
+
+
+/* Functions_Declarations */
+char *_strcat(char *dest, char *src);
+
+#endif /* MAIN_H */
+ 
