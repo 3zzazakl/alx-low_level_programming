6eef5bc7cc3e996698a62c73c688448aebf7f2bb
3zzazakl <vanzekovichgr@gmail.com>
2023-08-29 00:02:05 +0300

    memcopy

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,9 +4,9 @@
 /* Headers_Declarations */
 #include <stdio.h>
 
-
 /* Functions_Declaration */
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 
 #endif /* MAIN_H */
