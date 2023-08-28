eb21d9f47e6596b61545bc16dd09229eba55d21d
3zzazakl <vanzekovichgr@gmail.com>
2023-08-28 23:42:01 +0300

    Memset

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Headers_Declarations */
+#include <stdio.h>
+
+
+/* Functions_Declaration */
+char *_memset(char *s, char b, unsigned int n);
+
+
+#endif /* MAIN_H */
