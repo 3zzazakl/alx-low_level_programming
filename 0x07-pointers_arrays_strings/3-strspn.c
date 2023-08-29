cbcd052fca2611ffedf1db5eeacbdfb8de974910
3zzazakl <vanzekovichgr@gmail.com>
2023-08-29 05:34:02 +0300

    Strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,12 +4,12 @@
  * _strspn - gets the length of a prefix substring
  * @s: The input string.
  * @accept: The required string.
- * Return: (string representation of the string). 
+ * Return: (string representation of the string).
 */
 unsigned int _strspn(char *s, char *accept)
 {
 	int x, length, y;
-   
+
 	length = 0;
 	for (x = 0; s[x] != '\0'; x++)
 	{
