90204c0e5f506f3092ef98725777adce4ed5681b
3zzazakl <vanzekovichgr@gmail.com>
2023-08-29 05:22:17 +0300

    StrChr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,10 +3,13 @@
 
 /* Headers_Declarations */
 #include <stdio.h>
+#include <string.h>
+
 
 /* Functions_Declaration */
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 
 #endif /* MAIN_H */
