9faefea6ee1ef3e769582ac34fe94b205feccf77
3zzazakl <vanzekovichgr@gmail.com>
2023-08-30 05:58:25 +0300

    matr

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -4,19 +4,12 @@
 */
 void print_diagsums(int *a, int size)
 {
-       int x, y, value, main = 0, second = 0;
+       int x, main = 0, second = 0;
 
        for (x = 0; x < size; x++)
        {
-              for(y = 0; y < size; y++)
-              {
-                     value = a[x * size + y];
-
-                     if (x == y)
-                     {
-                            main += value;
-                     }
-              }
+              main += a[x * size + x];
+              second += a[x * size + (size - 1 - x)];
        }
        printf("%d%d\n", main, second);
 }
