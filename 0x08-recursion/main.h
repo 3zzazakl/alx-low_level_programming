f9e3e8dc1e67815638b96757f1a2157ce3ce2595
3zzazakl <vanzekovichgr@gmail.com>
2023-08-30 19:11:10 +0300

    Reverse

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,10 +2,11 @@
 #define MAIN_H
 
 /* Headers-Declarations */
-
+#include <stdio.h>
 
 /* Function_Declarations */
 int _putchar(char c);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 
 #endif /* MAIN_H */
