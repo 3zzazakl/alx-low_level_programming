3719fff84a0fe24d6aa7a945c19e4ff6594ef026
3zzazakl <vanzekovichgr@gmail.com>
2023-08-31 03:12:05 +0300

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,13 +11,21 @@ int is_prime_number(int n)
 		return (0);
 
 	else
-		return (isPrime(n, n / 2));  
+		return (isPrime(n, n / 2));
 }
 
+/**
+ * isPrime - Check if number is prime.
+ * @n: input number.
+ * @x: intermediate.
+ * Return: (1) is prime otherwise return (0).
+*/
 int isPrime(int n, int x)
 {
 	if (x == 1)
+	{
 		return (1);
+	}
 	else
 	{
 		if (n % 2 == 0)
