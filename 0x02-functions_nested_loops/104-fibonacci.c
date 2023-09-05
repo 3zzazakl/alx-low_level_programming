b88534d29705a1b9b608150d5948b8d3e6569e0e
3zzazakl <vanzekovichgr@gmail.com>
2023-09-05 04:58:16 +0300

    fibo

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include "stdio.h"
+
+/**
+ * main - prints the first 98 Fibonacci numbers.
+*/
+int main(void)
+{
+       long n1 = 0, n2 = 1, sum = 0, nxt;
+
+	while (sum < 89)
+	{
+		nxt = n1 + n2;
+
+		if (sum == 88)
+		{
+			printf("%lu", nxt);
+
+		}
+		else
+		{
+			printf("%lu, ", nxt);
+
+		}
+		n1 = n2;
+		n2 = nxt;
+		sum++;
+	}
+	printf("\n");
+	return (0);
+}
