9fcf676ef1182d3db757220644d1ff454b1fe831
3zzazakl <vanzekovichgr@gmail.com>
2023-09-06 03:09:38 +0300

    2d

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * alloc_grid - Print a pointer to a 2 dimensional array of integers.
+ * @width: the width of the grid
+ * @height: the height of the grid
+ * Return: ()
+*/
+int **alloc_grid(int width, int height)
+{
+	int **matrix, i, j;
+
+	if (width < 1 || height < 1)
+	{
+		return (NULL);
+	}
+
+	matrix = malloc(sizeof(int *) * height);
+
+	if (matrix == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		matrix[i] = malloc(sizeof(int) * width);
+
+		if (matrix[i] == NULL)
+		{
+			for (--i; i >= 0; i--)
+			{
+				free(matrix[i]);
+			}
+			free(matrix);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			matrix[i][j] = 0;
+		}
+	}
+	return (matrix);
+}
