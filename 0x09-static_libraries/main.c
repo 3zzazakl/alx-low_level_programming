5f8816fa2e838ffd9bb55defcfa54cc667e8a0d3
3zzazakl <vanzekovichgr@gmail.com>
2023-09-06 23:28:11 +0300

    static

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,14 @@
 #include "main.h"
 
+/**
+ * main - Printing a text.
+ * Return: (Success) 0.
+*/
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+const char a1 = "\"At the end of the day, my goal was to be the best ";
+const char a2 = "hacker\"\n\t- Kevin Mitnick";
+_puts(a1);
+_puts(a2);
+return (0);
 }
