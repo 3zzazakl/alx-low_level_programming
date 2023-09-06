55bff138539c98c097403b4de0344c8ac3f974c8
3zzazakl <vanzekovichgr@gmail.com>
2023-09-06 23:39:53 +0300

    Static

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -6,9 +6,6 @@
 */
 int main(void)
 {
-const char a1 = "\"At the end of the day, my goal was to be the best ";
-const char a2 = "hacker\"\n\t- Kevin Mitnick";
-_puts(a1);
-_puts(a2);
+_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 return (0);
 }
