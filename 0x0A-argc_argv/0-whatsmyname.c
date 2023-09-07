25e8e266b2b2298f49fe84cebdd05e34b740443c
3zzazakl <vanzekovichgr@gmail.com>
2023-09-07 04:41:33 +0300

    agrs

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,9 +2,12 @@
 
 /**
  * main - printing program name.
+ * @argc: Argument Count
+ * @argv: array of arguments.
+ * Return: (Success) 0
 */
 int main(int argc, char *argv[])
 {
-       printf("%s\n", argv[0]);
-       return (0);
+	printf("%s\n", argv[0]);
+	return (0);
 }
