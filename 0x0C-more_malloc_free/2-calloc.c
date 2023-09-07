d791eab409dab23ef73c6aea9dcc15bd9d8169e5
3zzazakl <vanzekovichgr@gmail.com>
2023-09-08 01:27:29 +0300

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an array.
+ * @nmemb: number of bytes to allocate.
+ * @size: size of memory
+ * Return: address.
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *ptr = malloc(nmemb * size);
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	memset(ptr, 0, nmemb * size);
+
+	return (ptr);
+}
