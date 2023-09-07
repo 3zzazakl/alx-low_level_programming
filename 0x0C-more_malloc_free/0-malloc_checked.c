98deb20f24e1d9f2e7ac3901a583411a7d7c0b82
3zzazakl <vanzekovichgr@gmail.com>
2023-09-07 21:01:55 +0300

    malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: Input type.
+ * Return: (Address of ptr)
+*/
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(sizeof(b));
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Headers-Declarations */
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/* Functions-Declarations */
+void *malloc_checked(unsigned int b);
+
+#endif /* MAIN_H */
