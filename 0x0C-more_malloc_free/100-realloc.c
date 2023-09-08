8f13d5517139dec251c1f4d1f90835a3c5b22fc5
3zzazakl <vanzekovichgr@gmail.com>
2023-09-08 04:06:29 +0300

    memorycopy

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _realloc - reallocates a memory block using malloc and free.
+ * @ptr: input pointer
+ * @old_size: Old size
+ * @new_size: new size
+ * Return: (ptr)
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *new_ptr;
+
+	if (new_size > old_size)
+	{
+		new_ptr = malloc(new_size);
+		free(ptr);
+		return (new_ptr);
+	}
+	else if (new_size == old_size)
+		return (ptr);
+	else if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+		free(ptr);
+		return (new_ptr);
+	}
+
+	else if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	return (ptr);
+}
