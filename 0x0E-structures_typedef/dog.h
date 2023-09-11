a4ec3141c2e9e1282713ba3e194d0fd112cf50b7
3zzazakl <vanzekovichgr@gmail.com>
2023-09-11 23:07:29 +0300

    dog.g

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,19 +20,6 @@ typedef struct dog
 	char *owner;
 } dog_t;
 
-/**
- * struct dog - declare a new struct.
- * @name: Name.
- * @age: Age.
- * @owner: Owner Name.
-*/
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-
 /* Fucntion_Declaration */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
