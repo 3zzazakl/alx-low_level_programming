973084a35d79e35b0275caa9eff6db793f2aef12
3zzazakl <vanzekovichgr@gmail.com>
2023-09-11 23:03:54 +0300

    dog.g

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,6 +19,13 @@ struct dog
 	char *owner;
 };
 
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
 /* Fucntion_Declaration */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
