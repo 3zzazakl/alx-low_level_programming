d6046eff1f794f8bd4884b7c9a542a9a81f21a06
3zzazakl <vanzekovichgr@gmail.com>
2023-09-11 21:43:34 +0300

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/* Struct-Declaration */
+/**
+ * struct dog - declare a new struct.
+ * @name: Name.
+ * @age: Age.
+ * @owner: Owner Name.
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
