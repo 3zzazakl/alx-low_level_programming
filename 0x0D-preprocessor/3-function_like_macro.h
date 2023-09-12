08900702cde9149b11914d87df227a60453d28d6
3zzazakl <vanzekovichgr@gmail.com>
2023-09-12 05:25:21 +0300

    macros

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,8 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO
 #define FUNCTION_LIKE_MACRO
 
-/* Headers_Declarations */
-
-#define ABS(x) ((x < 0) ? (-x) : x)
+#define ABS(x) ((x < 0) ? (-(x)) : x)
 
 #endif /* FUNCTION_LIKE_MACRO */
