d8e6eba393d92a0d3e127c3f8d2b0f79bd513f35
3zzazakl <vanzekovichgr@gmail.com>
2023-09-14 02:38:36 +0300

    name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - printing a string.
+ * @name: Input String.
+ * @f: Input Function pointer.
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/*Headers_Declarations*/
+
+
+/*Function_Decleations*/
+void print_name(char *name, void (*f)(char *));
+
+
+#endif /*FUNCTION_POINTERS_H*/
