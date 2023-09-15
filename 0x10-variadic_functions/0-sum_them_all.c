96c0d74e7cda8fe9043e0352ac951547dd153f3a
3zzazakl <vanzekovichgr@gmail.com>
2023-09-15 04:33:20 +0300

    sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,6 +7,11 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
+	if (n == 0)
+	{
+		return (0);
+	}
+
 	va_list args;
 
 	va_start(args, n);
@@ -22,10 +27,5 @@ int sum_them_all(const unsigned int n, ...)
 		}
 		va_end(args);
 		return (sum);
-	}
-
-	if (n == 0)
-	{
-		return (0);
 	}
 }
