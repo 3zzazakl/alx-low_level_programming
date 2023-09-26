0387d2c47c8f35380cf160ad181a9017db13d581
3zzazakl <vanzekovichgr@gmail.com>
2023-09-26 20:52:17 +0300

    free list using pointer funt

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free lists.
+ * @head: input list.
+ */
+void free_listint2(listint_t **head)
+{
+       listint_t *current;
+
+       while (*head != NULL)
+	{
+		current = *head;
+		*head = (*head)->next;
+		free(current);
+	}
+       *head = NULL;
+}
