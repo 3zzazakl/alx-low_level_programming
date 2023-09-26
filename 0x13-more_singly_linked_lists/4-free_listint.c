9944aca6bc0a493d6952de222bd7777a09a1aa6b
3zzazakl <vanzekovichgr@gmail.com>
2023-09-26 20:37:35 +0300

    free existing list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint - free the existing list
+ * @head: input list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *next, *current = head;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(next);
+		current = next;
+	}
+}
