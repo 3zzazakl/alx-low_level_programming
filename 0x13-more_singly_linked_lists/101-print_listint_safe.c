3d25d47d6711e28e39d1f9a5d454e883f7af36ba
3zzazakl <vanzekovichgr@gmail.com>
2023-09-27 03:46:32 +0300

    print list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,10 +7,10 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current;
+	const listint_t *current = head;
 	size_t count = 0;
 
-	current = head;
+
 	while (current != NULL)
 	{
 		printf("[%p] %d\n", (void *)current, current->n);
@@ -19,7 +19,7 @@ size_t print_listint_safe(const listint_t *head)
 		if (current->next >= current)
 		{
 			printf("-> [%p] %d\n", (void *) current->next, current->next->n);
-			break;
+			exit(98);
 		}
 		current = current->next;
 	}
