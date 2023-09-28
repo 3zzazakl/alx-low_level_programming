71446cc2722fd834f6a57cda5f5eba6f63fd506e
3zzazakl <vanzekovichgr@gmail.com>
2023-09-29 02:33:26 +0300

    decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: Input number.
+ * Return: (Decimal Number).
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int x = 0;
+
+	int y = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+
+	while (b[y] == '0' || b[y] == '1')
+	{
+		x <<= 1;
+		x += b[y] - '0';
+		y++;
+	}
+	return (x);
+
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*******Headers_Declarations****/
+#include <stdio.h>
+
+
+
+/******Structure_Declarations*********/
+
+
+
+
+
+/**********Functions_Declarations******/
+unsigned int binary_to_uint(const char *b);
+
+
+#endif /*MAIN_H*/
