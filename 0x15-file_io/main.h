ad6701d4c147868460c9d537c06d874542924869
3zzazakl <vanzekovichgr@gmail.com>
2023-10-03 03:29:36 +0300

    Creating a new file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <string.h>
 
 
 /******Struct_Declarations*******/
@@ -12,7 +15,7 @@
 
 /******Functions_Declarations*****/
 ssize_t read_textfile(const char *filename, size_t letters);
-
+int create_file(const char *filename, char *text_content);
 
 
 #endif /*MAIN_H*/
