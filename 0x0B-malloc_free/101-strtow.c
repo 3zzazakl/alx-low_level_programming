883a4a7e72d98824fd1f3bb9e54479b69b0de130
3zzazakl <vanzekovichgr@gmail.com>
2023-11-20 19:00:52 +0200

    split string to words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,25 +1,63 @@
 #include "main.h"
 
 /**
+ * count_words - splits string into words
+ * @str: input string
+ * Return: (Pointer to array of string).
+ */
+int count_words(char *str)
+{
+	int i = 0, count = 0, length;
+
+	length = strlen(str);
+	while (i < length)
+	{
+		while (i < length && str[i] == ' ')
+			i++;
+		if (i < length && str[i] != ' ')
+		{
+			count++;
+			while (i < length && str[i] != ' ')
+				i++;
+		}
+	}
+	return (count);
+}
+/**
  * strtow - splits a string into words.
  * @str: Input String.
  * Return: (Splitted words).
 */
 char **strtow(char *str)
 {
-	char *token;
+	int i = 0, j = 0, index = 0, length;
+	char **words;
 
-	if (str == NULL ||  *str == '\0')
-	{
+	length = strlen(str);
+	if (str == NULL || str[0] == '\0')
 		return (NULL);
-	}
+	words = malloc((count_words(str) + 1) * sizeof(char *));
 
-	token = strtok(str, " ");
-
-	while (token != NULL)
+	if (words == NULL)
+		return (NULL);
+	while (i < length)
 	{
-		printf("%s\n", token);
-		token = strtok(NULL, " ");
+		while (i < length && str[i] == ' ')
+			i++;
+		if (i < length && str[i] != ' ')
+		{
+			j = i;
+			while (i < length && str[i] != ' ')
+				i++;
+			words[index] = malloc((i - j + 1) * sizeof(char));
+			if (words[index] == NULL)
+				return (NULL);
+			strncpy(words[index], &str[j], i - j);
+			words[index][i - j] = '\0';
+			index++;
+		}
 	}
-	return (0);
+	words[index] = NULL;
+	return (words);
 }
+
