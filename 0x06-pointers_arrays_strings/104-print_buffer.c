422c423dab086a6828fd4f7992fda70ab1b95f2c
3zzazakl <vanzekovichgr@gmail.com>
2023-11-20 17:21:47 +0200

    print line

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -9,20 +9,20 @@ void print_line(char *c, int s, int l)
 {
 	int j, k;
 
-	for (j = 0; j <= 10; j++)
+	for (j = 0; j < 10; j++)
 	{
 		if (j < s)
-			printf("%02x", (unsigned char)c[l * 10 + j]);
+			printf("%02x", c[l * 10 + j]);
 		else
 			printf(" ");
-	/* 	if (j % 2)
-			putchar(' '); */
+		if (j % 2)
+			putchar(' ');
 	}
-	printf(" ");
-
-	for (k = 0; k < s; k++)
+/* 	printf(" ");
+ */
+	for (k = 0; k < 10; k++)
 	{
-		if (c[l * 10 + k] >= 32 && c[l * 10 + k] <= 126)
+		if (k < s && c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
 			putchar(c[l * 10 + k]);
 		else
 			putchar('.');
@@ -38,13 +38,13 @@ void print_buffer(char *b, int size)
 {
 	int i;
 
-	for (i = 0; i <= (size - 1) / 10 && size; i++)
+	for (i = 0; i < (size + 9) / 10 && size; i++)
 	{
 		printf("%08x: ", i * 10);
-		if (i < size / 10)
-			print_line(b, 9, i);
+		if (i * 10 < size)
+			print_line(b, size - i * 10, i);
 		else
-			print_line(b, size % 10 - 1, i);
+			print_line(b, 0, i);
 		putchar('\n');
 	}
 	if (size == 0)
