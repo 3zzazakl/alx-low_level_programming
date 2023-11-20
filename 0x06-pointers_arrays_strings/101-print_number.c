20279cd4c2c1564526909947efb7114962aa9617
3zzazakl <vanzekovichgr@gmail.com>
2023-11-20 16:23:59 +0200

    putchar

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_number - print numbers using _putchar
+ * @n: input number
+ */
+void print_number(int n)
+{
+	unsigned int number;
+
+	if (n < 0)
+	{
+		number = -n;
+		_putchar('-');
+	}
+	else
+		number = n;
+	if (number / 10)
+		print_number(number / 10);
+	_putchar((number % 10) + '0');
+}
