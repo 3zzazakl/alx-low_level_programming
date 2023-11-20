3da6729eb3f3cef5e32b34f6489091092f5c68ae
3zzazakl <vanzekovichgr@gmail.com>
2023-11-20 15:21:43 +0200

    encoding a string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,21 +6,17 @@
 */
 char *leet(char *s)
 {
-	int i;
+	int i, j;
+	char let[8] = {'0', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i])
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		else if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		else if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		else if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		else if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (j = 0; j <= 7; j++)
+		{
+			if (s[i] == let[j] || s[i] - 32 == let[j])
+				s[i] = j + '0';
+		}
+		i++;
 	}
-
 	return (s);
 }
