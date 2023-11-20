70e6c26e123d6652b01680465b1989934e0eab12
3zzazakl <vanzekovichgr@gmail.com>
2023-11-20 17:36:43 +0200

    print line

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -19,6 +19,5 @@ char *leet(char *);
 void print_number(int n);
 void reverse_string(char *n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_line(char *c, int s, int l);
 void print_buffer(char *b, int size);
 #endif /* MAIN_H */
