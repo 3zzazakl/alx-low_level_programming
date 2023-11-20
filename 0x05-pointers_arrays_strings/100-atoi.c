32b2f7b31bf0cb99530a2f0f5c2c52c619cf53b0
3zzazakl <vanzekovichgr@gmail.com>
2023-11-20 14:54:57 +0200

    atio

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,25 +8,18 @@
 */
 int _atoi(char *s)
 {
-	int result = 0;
 	int sign = 1;
+	unsigned int num = 0;
 
-	while (isspace(*s))
-	{
-		s++;
-	}
-
-	if (*s == '-' || *s == '+')
-	{
+	do {
 		if (*s == '-')
-		sign = -1;
-		s++;
-	}
-
-	while (isdigit(*s))
-	{
-		result = result * 10 + (*s - '0');
-		s++;
-	}
-	return (result * sign);
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+		{
+			num = (num * 10) + (*s - '0');
+		}
+		else if (num > 0)
+			break;
+	} while (*s++);
+	return (num * sign);
 }
