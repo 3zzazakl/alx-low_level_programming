4307caa3fdca77c62d8f58c6b3922103a1479a2a
3zzazakl <vanzekovichgr@gmail.com>
2023-12-02 02:33:28 +0200

    fix err

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,31 +1,36 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - This function deletes a hash table
+ * hash_table_delete - function deletes a hash table
  * @ht: The hash table to be deleted
  *
  * Return: Nothing
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *curr, *temp;
+	hash_node_t *current, *temp;
 	unsigned long int i;
-
-	if (ht == NULL)
-		return;
-
-	for (i = 0; i < ht->size; i++)
+	
+	if (ht != NULL)
 	{
-		curr = ht->array[i];
-		while (curr != NULL)
+		for (i = 0; i < ht->size; i++)
 		{
-			temp = curr;
-			curr = curr->next;
-			free(temp->key);
-			free(temp->value);
-			free(temp);
+			if (ht->array != NULL)
+			{
+				current = ht->array[i];
+				
+				while (current != NULL)
+				{
+					temp = current;
+					current = current->next;
+					free(temp->key);
+					free(temp->value);
+					free(temp);
+				}
+			}
 		}
-	}
 	free(ht->array);
 	free(ht);
+	}
+
 }
